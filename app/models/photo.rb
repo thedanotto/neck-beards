989770b6aa0292e2377446fa8e8559c0a309c2e7
Matthew Krog <mdkrog@gmail.com>
2016-01-21 11:12:58 +0200

    Create Photo Model
    * Photos contain two fields
      * Url (string and required)
      * Caption (string and not required)
    * Add a belongs to user relation
    * Add foreign key to keep database integrity
    * Add a has_many photos relation to User

---
+++ app/models/photo.rb
@@ -0,0 +1,5 @@
+class Photo < ActiveRecord::Base
+  belongs_to :user
+
+  validates_presence_of :url
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,13 @@
-FactoryGirl.define do  factory :timeline do
-    
-  end
-  factory :user do
-    
+FactoryGirl.define do
+  factory :photo do
+    user
+    url "http://img1.sunset.timeinc.net/sites/default/files/image/2010/06/stanley-lake-0610-x.jpg"
+    caption "Mountains and a lake"
   end
 
+  factory :timeline do
+  end
+
+  factory :user do
+  end
 end
