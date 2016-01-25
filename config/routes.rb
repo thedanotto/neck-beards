ea80779883d1e0069ab96716ce5fb2691e7ec950
Patrick Rebel <leberpj@outlook.com>
2016-01-25 02:45:54 +0000

    Extra cleanup for Devise versus Monban

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
+  root to: "timelines#index", via: :get
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  root to: "timelines#index"
-
-  resources :timelines, only: [:index]
-  # resources :photos, only: [:show, :new, :create]
 end
