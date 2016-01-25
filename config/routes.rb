0b904b05a0df61fbae1a0cdf0798ddfb7c9ab3fb
Patrick Rebel <leberpj@outlook.com>
2016-01-25 14:36:28 +0000

    I setup Monban for user authentication with the photo model and controller code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   root to: "timelines#index"
   resources :timelines, only: [:index]
   resources :photos, only: [:show, :new, :create]
