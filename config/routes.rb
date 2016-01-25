ccb8d23ab6092afae9dc3e8d66e72c798f9e45ad
Patrick Rebel <leberpj@outlook.com>
2016-01-25 15:56:24 +0000

    Add files and folders for photo model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root to: "timelines#index", via: :get
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  resources :timelines, only: [:index]
   resources :photos, only: [:show, :new, :create]
 end
