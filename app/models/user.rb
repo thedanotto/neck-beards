9134d6c2b029b30ccaf2c70dc42f8047850018c3
Jesús Burgos <jesus@visrez.com>
2016-01-30 18:19:09 +0100

    Fixes user registration and adds feature tests. Because:
    - I wasn't able to sign up, I was always getting "full name can't be blank".
    
    This commit:
    
    - Adds a form field so the full name can be filled in.
    - Adds other optional fields for signing up.
    - Adds a feature test for sign ups:
      - Users can sign up.
      - Duplicate usernames are rejected.
    - Adds a flash message when a sign up is successful.
    - Sets the maximum bio length on a constant so it can be used in the view.
    - Changes the validation for username, when present it must be unique, without
      case sensitiveness.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
+  MAX_BIO_LENGTH = 140
   has_many :photos
 
-  validates :bio, length: { maximum: 140 }
+  validates :bio, length: { maximum: MAX_BIO_LENGTH }
   validates :email, presence: true, uniqueness: true
   validates :full_name, presence: true
   validates :password_digest, presence: true
-  validates :username, uniqueness: { case_sensitive: true }
+  validates :username, uniqueness: { case_sensitive: false, allow_blank: true }
 end
